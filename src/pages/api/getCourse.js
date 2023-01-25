0abe7dcef2fc00eb47e5207636c00654a9dec619
rishabhbizzle <rishabhbizzle@gmail.com>
2023-01-25 18:52:20 +0530

    test5

--- src/pages/api/getCourse.js
+++ src/pages/api/getCourse.js
@@ -1,23 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 
 
 export default async function handler(req, res) {
   // console.log
   // console.log(req.query)
-  let api = await fetch("https://jsonplaceholder.typicode.com/todos");
-  // console.log(api.status);
-  let data = await api.json();
-  return res.status(200).json(data);
-
-  // fs.readFile(`jsondata/${req.query.course}.json`, 'utf-8', (err, data) => {
-  //   if (err){
-  //     console.log(err)
-  //     return res.status(404).json({"error": true, "message": "Course not found"})
-  //   }
-  //   // console.log(data)
-  //   return res.status(200).json(JSON.parse(data));
-  // })
+  const jsonDirectory = path.join(process.cwd(), 'jsondata');
+  console.log(jsonDirectory)
+  const contents = await fs.readFile(jsonDirectory + `/${req.query.course}.json`, 'utf-8');
+  return res.status(200).json(JSON.parse(contents));
   
 }
