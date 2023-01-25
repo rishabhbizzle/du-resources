8bf86dd5204190c8f560a4f9d385fcb6c8d2a543
rishabhbizzle <rishabhbizzle@gmail.com>
2023-01-25 17:43:48 +0530

    test4

--- src/pages/api/getCourse.js
+++ src/pages/api/getCourse.js
@@ -6,13 +6,18 @@ import path from 'path';
 export default async function handler(req, res) {
   // console.log
   // console.log(req.query)
-  fs.readFile(`jsondata/${req.query.course}.json`, 'utf-8', (err, data) => {
-    if (err){
-      console.log(err)
-      return res.status(404).json({"error": true, "message": "Course not found"})
-    }
-    // console.log(data)
-    return res.status(200).json(JSON.parse(data));
-  })
+  let api = await fetch("https://jsonplaceholder.typicode.com/todos");
+  // console.log(api.status);
+  let data = await api.json();
+  return res.status(200).json(data);
+
+  // fs.readFile(`jsondata/${req.query.course}.json`, 'utf-8', (err, data) => {
+  //   if (err){
+  //     console.log(err)
+  //     return res.status(404).json({"error": true, "message": "Course not found"})
+  //   }
+  //   // console.log(data)
+  //   return res.status(200).json(JSON.parse(data));
+  // })
   
 }
--- src/pages/api/getSubjectResources.js
+++ src/pages/api/getSubjectResources.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const subject = req.query.sub;
   fs.readFile(`jsondata/${subject}.json`, 'utf-8', (err, data) => {
     if (err){
-    //   console.log(err)
+      console.log(err)
       return res.status(500).json({ error: "No such subject found" })
     }
     // console.log(data)
